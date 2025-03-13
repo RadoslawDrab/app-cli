f44b6bafd919aadef6e50bcc1a75ee23dad7db85
RadoslawDrab <radoslaw.drab03@gmail.com>
2025-03-13 13:38:11 +0100

    added new esbuild env variables

--- src/build/esbuild.ts
+++ src/build/esbuild.ts
@@ -1,25 +1,31 @@
 import * as esbuild from 'esbuild'
+import fs from 'fs'
 import path from 'path'
 import { Build, log } from 'build'
 
-export default (build: Build) => {
-    try {
-        const options: esbuild.BuildOptions = {
-            entryPoints: ['./src/index.ts'],
-            outfile: path.resolve(build.CURRENT_DIR, build.OUT_FILE),
-            bundle: true,
-            format: 'cjs',
-            platform: 'node',
-            treeShaking: true,
-            target: 'esnext',
-            minify: true,
-            sourcemap: 'inline',
-        }
+export const ENV_VARIABLES = ['NODE_ENV', 'VERSION']
 
-        log('default', 'Started building...')
-        esbuild.buildSync(options)
-        log('info', 'Package built')
-    } catch (e: any) {
-        log('error', 'Error:', e)
+export default (build: Build) => {
+    const env = process.env
+    const packageFile = path.join(process.cwd(), 'package.json')
+    if (fs.existsSync(packageFile)) env.VERSION = JSON.parse(fs.readFileSync(packageFile, 'utf8')).version
+
+    const options: esbuild.BuildOptions = {
+        entryPoints: ['./src/index.ts'],
+        outfile: path.resolve(build.CURRENT_DIR, build.OUT_FILE),
+        bundle: true,
+        format: 'cjs',
+        platform: 'node',
+        treeShaking: true,
+        target: 'esnext',
+        minify: true,
+        sourcemap: 'inline',
+        define: Object.keys(env)
+                      .filter(key => ENV_VARIABLES.includes(key))
+                      .reduce((obj, key) => ({ ...obj, ['process.env.' +  key]: `"${env[key]}"`}), {})
     }
+
+    log('default', 'Started building...')
+    esbuild.buildSync(options)
+    log('info', 'Package built')
 }
