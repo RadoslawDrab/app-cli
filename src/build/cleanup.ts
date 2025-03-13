54050b7d27651790bebcb78a11406b862deba638
RadoslawDrab <radoslaw.drab03@gmail.com>
2025-03-13 13:37:26 +0100

    removed try catch

--- src/build/cleanup.ts
+++ src/build/cleanup.ts
@@ -2,11 +2,7 @@ import { Build, log } from 'build'
 import fs from 'fs'
 
 export default (build: Build) => {
-    try {
-        log('default', 'Cleanup started')
-        fs.rmSync(build.OUT_DIR, { recursive: true, force: true })
-        log('info', 'Cleanup completed')
-    } catch (e: any) {
-        log('error', 'Error:', e)
-    }
+    log('default', 'Cleanup started')
+    fs.rmSync(build.OUT_DIR, { recursive: true, force: true })
+    log('info', 'Cleanup completed')
 }
--- src/build/package.ts
+++ src/build/package.ts
@@ -3,20 +3,16 @@ import path from 'path'
 import { Build, log } from 'build'
 
 export default (build: Build) => {
-    try {
-        const scriptName = path.basename(build.CURRENT_DIR)
+    const scriptName = path.basename(build.CURRENT_DIR)
 
-        const packageContent = JSON.parse(fs.readFileSync(path.join(build.CURRENT_DIR, 'package.json')).toString())
+    const packageContent = JSON.parse(fs.readFileSync(path.join(build.CURRENT_DIR, 'package.json')).toString())
 
-        packageContent.bin = {
-            ...(packageContent.bin ?? {}),
-            [scriptName]: path.relative(process.cwd(), build.OUT_FILE),
-        }
-        packageContent.name = scriptName
-
-        fs.writeFileSync(path.join(build.CURRENT_DIR, 'package.json'), JSON.stringify(packageContent, null, 2))
-        log('info', 'package.json modified')
-    } catch (e: any) {
-        log('error', 'Error:', e)
+    packageContent.bin = {
+        ...(packageContent.bin ?? {}),
+        [scriptName]: path.relative(process.cwd(), build.OUT_FILE),
     }
+    packageContent.name = scriptName
+
+    fs.writeFileSync(path.join(build.CURRENT_DIR, 'package.json'), JSON.stringify(packageContent, null, 2))
+    log('info', 'package.json modified')
 }
